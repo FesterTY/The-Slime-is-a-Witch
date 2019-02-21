0692713e4266544847f55ef02cea46adfe807600
Fester <festersy@gmail.com>
2019-02-21 14:45:27 +0700

    Added assets and PlayerController for horizontal movement

---
+++ The Stakes Are High/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+
+    PlayerController controller;
+    Vector2 moveVelocity;
+
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        moveVelocity = moveInput.normalized * moveSpeed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+
+        controller.Move(moveVelocity);
+    }
+}
---
+++ The Stakes Are High/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 _velocity)
+    {
+        rb2d.MovePosition(rb2d.position + _velocity);
+    }
+}
