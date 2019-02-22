2dbb593dcbc03b39a6065d481911cf4b786eef84
Fester <festersy@gmail.com>
2019-02-22 19:45:30 +0700

    Implemented a simple health system

---
+++ The Stakes Are High/Assets/Scripts/Living Entities/HealthManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxHealth;
+    [HideInInspector]
+    public int currentHealth;
+
+    public void SetFullHealth()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void Harm(int _damageAmount)
+    {
+        currentHealth -= _damageAmount;
+    }
+}
