550ca52281a781d743da83ac0d08d68a3831cad1
Fester <festersy@gmail.com>
2019-02-27 20:36:05 +0700

    Player now fall to death, new "well" prop & changes to jumping animations

---
+++ The Stakes Are High/Assets/RestartLevelOnTouch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RestartLevelOnTouch : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D _collision)
+    {
+        if (_collision.tag == "Player")
+        {
+            HealthManager healthManager = _collision.GetComponent<HealthManager>();
+
+            // Harming by the current health gets the player's health to 0
+            healthManager.Harm(healthManager.currentHealth);
+        }
+    }
+
+}
